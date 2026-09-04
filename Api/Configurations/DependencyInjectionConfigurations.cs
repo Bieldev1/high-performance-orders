39da0f850ec5641f6bba0e9454ce7fead4b78791
Gabriel Rocha Santos <gabriel.santos@vikservices.com.br>
2026-09-04 16:22:57 -0300

    feat: wire up dependency injection and add initial EF Core migration

---
+++ Api/Configurations/DependencyInjectionConfigurations.cs
@@ -0,0 +1,23 @@
+using Domain.AggregatesModel.ClienteAggregate;
+using Domain.AggregatesModel.PedidoAggregate;
+using Domain.SeedWork;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Configurations;
+
+public static class DependencyInjectionConfigurations
+{
+    public static IServiceCollection AddDependencyInjectionConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IPedidoRepository, PedidoRepository>();
+        services.AddScoped<IClienteRepository, ClienteRepository>();
+
+        return services;
+    }
+}
